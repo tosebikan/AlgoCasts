2c39a6508232ce895bb6203782fd7853d14ff7ab
tosebikan <tosebikan@gmail.com>
2021-02-10 09:01:20 +0000

    solve maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,48 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+// refactor
+function maxChar(str) {
+  let hash = {};
+  let max = 0;
+  let maxChar = "";
+
+  for (let char of str) {
+    hash[char] = hash[char] + 1 || 1;
+  }
+
+  for (let char in hash) {
+    if (hash[char] > max) {
+      max = hash[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
+// using an hashmap
+// function maxChar(str) {
+//   let hash = {};
+//
+//   let arr = str.split("");
+//
+//   for (let char of arr) {
+//     if (hash[char]) {
+//       hash[char]++;
+//     } else {
+//       hash[char] = 1;
+//     }
+//   }
+//
+//   let maxChar = "";
+//   for (let char in hash) {
+//     console.log(char, hash[char]);
+//     if (hash[char] > maxChar) {
+//       maxChar = char;
+//     }
+//   }
+//
+//   return maxChar;
+// }

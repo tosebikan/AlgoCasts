9426f2686371f6ca09b4245ad8b766bc908f0983
tosebikan <tosebikan@gmail.com>
2021-02-12 09:24:34 +0000

    solve chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,34 +8,63 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-// first approach
+// using slice
 function chunk(array, size) {
-  // create the result array
-  // create a sub array
-  let res = [];
-  let subArr = [];
+  let chunked = [];
 
-  // loop through array
-  for (let i = 0; i <= array.length - 1; i++) {
-    // if sub array length is less than size push index element
-    // if full push to res empty sub and add that element
-    if (subArr.length < size) {
-      subArr.push(array[i]);
-    } else if ((subArr.length = size)) {
-      res.push(subArr);
-      subArr = [];
-      subArr.push(array[i]);
-    }
-    // push sub array into result subarray
+  let index = 0;
+
+  while (index < array.length) {
+    chunked.push(array.slice(index, index + size));
+    index += size;
   }
-
-  // if sub array still exists add to res
-  if (subArr.length > 0) {
-    res.push(subArr);
-  }
-
-  // return result array
-  return res;
+  return chunked;
 }
 
 module.exports = chunk;
+// reduced
+// function chunk(array, size) {
+//   let chunked = [];
+//
+//   for (let element of array) {
+//     let last = chunked[chunked.length - 1];
+//
+//     if (!last || last.length === size) {
+//       chunked.push([element]);
+//     } else {
+//       last.push(element);
+//     }
+//   }
+//
+//   return chunked;
+// }
+
+// first approach
+// function chunk(array, size) {
+//   // create the result array
+//   // create a sub array
+//   let res = [];
+//   let subArr = [];
+//
+//   // loop through array
+//   for (let i = 0; i <= array.length - 1; i++) {
+//     // if sub array length is less than size push index element
+//     // if full push to res empty sub and add that element
+//     if (subArr.length < size) {
+//       subArr.push(array[i]);
+//     } else if ((subArr.length = size)) {
+//       res.push(subArr);
+//       subArr = [];
+//       subArr.push(array[i]);
+//     }
+//     // push sub array into result subarray
+//   }
+//
+//   // if sub array still exists add to res
+//   if (subArr.length > 0) {
+//     res.push(subArr);
+//   }
+//
+//   // return result array
+//   return res;
+// }

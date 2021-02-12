9376bbf36855fccd6ae65d1dacaf1b6e58702855
tosebikan <tosebikan@gmail.com>
2021-02-12 08:40:02 +0000

    solve chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,34 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// first approach
+function chunk(array, size) {
+  // create the result array
+  // create a sub array
+  let res = [];
+  let subArr = [];
+
+  // loop through array
+  for (let i = 0; i <= array.length - 1; i++) {
+    // if sub array length is less than size push index element
+    // if full push to res empty sub and add that element
+    if (subArr.length < size) {
+      subArr.push(array[i]);
+    } else if ((subArr.length = size)) {
+      res.push(subArr);
+      subArr = [];
+      subArr.push(array[i]);
+    }
+    // push sub array into result subarray
+  }
+
+  // if sub array still exists add to res
+  if (subArr.length > 0) {
+    res.push(subArr);
+  }
+
+  // return result array
+  return res;
+}
 
 module.exports = chunk;

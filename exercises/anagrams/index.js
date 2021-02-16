2a25d7cd565af3a69340ddeda671d50f15d97f29
tosebikan <tosebikan@gmail.com>
2021-02-16 07:22:23 +0000

    solve anagrams with a hash

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -10,27 +10,62 @@
 
 // remove spaces, punctuations & convert to lower case
 
-// first approach
+// second approach using a hash
 function helper(string) {
-  //remove punctuations make lower case & remove spaces
-  // const punctuations = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+  let hash = {};
 
-  // remove all punctuation & spaces
-  const regex = /[^\w]/g;
+  // remove the punctuations & white spaces
+  let regex = /[^\w]/g;
+  let newString = string.replace(regex, "");
 
-  return (newStr = string
-    .replace(regex, "")
-    .toLowerCase()
-    .split("")
-    .sort()
-    .join(""));
+  for (let char of newString) {
+    hash[char] = hash[char] + 1 || 1;
+  }
+  return hash;
 }
 
 function anagrams(stringA, stringB) {
+  // create a helper function to create hash for the strings
   let newStringA = helper(stringA);
   let newStringB = helper(stringB);
 
-  return newStringA === newStringB ? true : false;
+  // compare the lengths & return false if not equal
+  if (Object.keys(newStringA).length !== Object.keys(newStringB).length) {
+    return false;
+  }
+
+  // loop through one of the maps & compare with the another
+  for (char in newStringA) {
+    if (newStringA[char] !== newStringB[char]) {
+      return false;
+    }
+  }
+
+  return true;
+
+  console.log({ newStringA, newStringB });
 }
 
 module.exports = anagrams;
+
+// function helper(string) {
+//   //remove punctuations make lower case & remove spaces
+//   // const punctuations = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+//
+//   // remove all punctuation & spaces
+//   const regex = /[^\w]/g;
+//
+//   return (newStr = string
+//     .replace(regex, "")
+//     .toLowerCase()
+//     .split("")
+//     .sort()
+//     .join(""));
+// }
+//
+// function anagrams(stringA, stringB) {
+//   let newStringA = helper(stringA);
+//   let newStringB = helper(stringB);
+//
+//   return newStringA === newStringB;
+// }

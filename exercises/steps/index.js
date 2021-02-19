3ec2da6ed4167570b828f82d60966ae46561b23a
tosebikan <tosebikan@gmail.com>
2021-02-19 09:46:47 +0000

    solve steps using a recursive function

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,21 +17,42 @@
 //       '### '
 //       '####'
 
-// first attempt with a nested loop
-function steps(n) {
-  let arr = [];
-
-  for (let i = 1; i <= n; i++) {
-    let str = "";
-
-    for (j = 1; j <= i; j++) {
-      str += "#";
-    }
-
-    console.log(str.padEnd(n));
+//using a recursion
+function steps(n, row = 0, stairs = "") {
+  if (n === row) {
+    return;
   }
+
+  if (n === stairs.length) {
+    console.log(stairs);
+    steps(n, row + 1);
+    return;
+  }
+
+  if (stairs.length <= row) {
+    stairs += "#";
+  } else {
+    stairs += " ";
+  }
+
+  steps(n, row, stairs);
 }
 
-steps(2);
+// steps(2);
 
 module.exports = steps;
+
+// first attempt with a nested loop
+// function steps(n) {
+//   let arr = [];
+//
+//   for (let i = 1; i <= n; i++) {
+//     let str = "";
+//
+//     for (j = 1; j <= i; j++) {
+//       str += "#";
+//     }
+//
+//     console.log(str.padEnd(n));
+//   }
+// }

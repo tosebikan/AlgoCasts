23c56e56c489ae220d43be6b9f4f9bd3b91dc4c3
tosebikan <tosebikan@gmail.com>
2021-03-04 08:48:45 +0000

    solve pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,61 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  // loop though
+  let midpoint = Math.floor((2 * n - 1) / 2);
+  for (let row = 0; row < n; row++) {
+    let level = "";
+
+    // loop through the column in row
+    for (let column = 0; column < 2 * n - 1; column++) {
+      // get midpoint
+      if (midpoint - row <= column && midpoint + row >= column) {
+        level += "#";
+      } else {
+        level += " ";
+      }
+    }
+    console.log(level);
+  }
+}
 
 module.exports = pyramid;
+
+// function pyramid(n) {
+//   // first loop through the number
+//   let str = "";
+//   let halfDiff = "";
+//   let diff = "";
+//
+//   for (let i = 1; i <= n; i++) {
+//     let max = n + (n - 1);
+//     let cur = i + (i - 1);
+//
+//     for (let j = 1; j <= max; j++) {
+//       // halfDiff = Math.floor((max - j) / 2);
+//       diff = max - cur;
+//       halfDiff = (max - cur) / 2;
+//
+//       if (j <= cur) {
+//         str += "#";
+//       } else if (diff == 0) {
+//         return;
+//       } else if (diff) {
+//         for (let i = 1; i <= diff; i++) {
+//           if (i <= halfDiff) {
+//             str = "0" + str;
+//           } else {
+//             str += "0";
+//           }
+//         }
+//       }
+//     }
+//
+//     console.log({ n, str, max, halfDiff, diff });
+//     str = "";
+//   }
+//
+//   // if less that total add spaces on both sides
+//   // else add the ppound sign
+// }

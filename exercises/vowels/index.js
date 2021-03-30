a58134acec62260d5a0704f857570fb6f1d907ab
tosebikan <tosebikan@gmail.com>
2021-03-30 06:57:47 +0000

    solve vowels

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,20 +7,29 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
+// using regex
 function vowels(str) {
-  let counter = 0;
+  let regex = /[aeiou]/gi;
 
-  // loop through string
-  for (let i of str) {
-    // check if array includes the string and increase the counter
-    if (["a", "e", "i", "o", "u"].includes(i.toLowerCase())) {
-      counter++;
-    }
-  }
+  let matches = str.match(regex);
 
-  return counter;
-
-  // print counter
+  return matches ? matches.length : 0;
 }
 
 module.exports = vowels;
+
+// using a loop
+// function vowels(str) {
+//   let counter = 0;
+//
+//   // loop through string
+//   for (let i of str) {
+//     // check if array includes the string and increase the counter
+//     if (["a", "e", "i", "o", "u"].includes(i.toLowerCase())) {
+//       counter++;
+//     }
+//   }
+//
+//   return counter;
+//   // print counter
+// }
